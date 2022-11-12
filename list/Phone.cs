97ea52e4ab73000d40af338fccacf75b9c2cc656
viktor <moiseev0506@mail.ru>
2022-11-12 21:22:40 +0300

    version 1

---
+++ list/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Phone
+    {
+        public string model { get; set; }
+        public int memory { get; set; }
+        public int camera { get; set; } 
+        public string proccesor { get; set; }
+    }
+}
---
+++ list/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Phone> list = new List<Phone>();
+            Phone phone = new Phone();
+
+            Console.Write("Введите название модели: ");
+            phone.model = Console.ReadLine();
+            Console.Write("Введите кол-во памяти: ");
+            phone.memory = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во мегапикселей: ");
+            phone.camera = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите название процессора: ");
+            phone.proccesor = Console.ReadLine();
+
+            list.Add(phone);
+            foreach(var item in list)
+            {
+                Console.WriteLine("Model: "+item.model);
+                Console.WriteLine("Memory: "+item.memory);
+                Console.WriteLine("Camera: "+item.camera);
+                Console.WriteLine("Proccesor: "+item.proccesor);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
